0e0bc043856a53e6f47fd533873eecf0d67bdc1d
Lee Jinsol <n.solee9@gmail.com>
2023-10-16 14:51:12 +0900

    Update connectionController.js

--- controllers/connectionController.js
+++ controllers/connectionController.js
@@ -2,7 +2,7 @@ const { friendModel, alarmModel } = require('../models/connectionModel');
 
 const friendController = {
     friendList: (req, res) => {
-        const mem_code = parseInt(req.session.user_code);
+        const mem_code = req.session.user_code;
         friendModel.getFriends(mem_code, (err, result) => {
             if (err) {
                 console.log(err)
@@ -27,7 +27,7 @@ const friendController = {
     },
 
     pendingList: (req, res) => {
-        const mem_code = parseInt(req.session.user_code);
+        const mem_code = req.session.user_code;
         friendModel.pendingList(mem_code, (err, result) => {
             if (err) {
                 console.log(err)
@@ -38,8 +38,8 @@ const friendController = {
     },
 
     acceptedFriend: (req, res) => {
-        const friend_code = parseInt(req.body.friend_code);
-        const login_code = parseInt(req.session.user_code);
+        const friend_code = req.body.friend_code;
+        const login_code = req.session.user_code;
 
         friendModel.acceptedFriend(friend_code, login_code, (err, result) => {
             if (err) {
@@ -53,8 +53,8 @@ const friendController = {
     },
 
     rejectFriend: (req, res) => {
-        const friend_code = parseInt(req.body.friend_code);
-        const login_code = parseInt(req.session.user_code);
+        const friend_code = req.body.friend_code;
+        const login_code = req.session.user_code;
 
         friendModel.rejectFriend(friend_code, login_code, (err, result) => {
             if (err) {
@@ -68,8 +68,8 @@ const friendController = {
     },
 
     cancelFriend: (req, res) => {
-        const friend_code = parseInt(req.body.friend_code);
-        const login_code = parseInt(req.session.user_code);
+        const friend_code = req.body.friend_code;
+        const login_code = req.session.user_code;
 
         friendModel.rejectFriend(login_code, friend_code, (err, result) => {
             if (err) {
@@ -83,8 +83,8 @@ const friendController = {
     },
     
     deleteFriend: (req, res) => {
-        const friend_code = parseInt(req.body.friend_code);
-        const login_code = parseInt(req.session.user_code);
+        const friend_code = req.body.friend_code;
+        const login_code = req.session.user_code;
         friendModel.deleteFriend(friend_code, login_code, (err, result) => {
             if (err) {
                 console.log(err);
