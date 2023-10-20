c4c21e8a3029b630760d9b28e20ca2d2ac3ba979
b1uey0 <3_14km@naver.com>
2023-10-21 06:45:21 +0900

    index 반응형

--- public/javascript/header.js
+++ public/javascript/header.js
@@ -3,5 +3,4 @@ const navToggleButton = document.querySelector(".nav-toggle-button");
 
 navButton.addEventListener("click", () => {
   navToggleButton.classList.toggle("active"); // active 클래스 토글
-  console.log("Button clicked"); // 클릭 이벤트 발생 시 로그 출력
 });
